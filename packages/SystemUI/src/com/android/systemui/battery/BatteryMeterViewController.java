48debf4b3ba940e58c2c00e7db8e04e984941edb
Michael Bestas <mkbestas@lineageos.org>
2023-03-14 22:46:43 +0200

    Revert "SystemUI: Bring back good ol' circle battery style"
    Conflicts with QPR2 changes
    
    This reverts commit e18e6fef84daf682bf511e75a7cb4b1161158063.
    
    Change-Id: Ifeaa0a543fe268235f205de2dada7c4cee6cb89a

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -15,8 +15,7 @@
  */
 package com.android.systemui.battery;
 
-import static lineageos.providers.LineageSettings.System.STATUS_BAR_BATTERY_STYLE;
-import static lineageos.providers.LineageSettings.System.STATUS_BAR_SHOW_BATTERY_PERCENT;
+import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -40,8 +39,6 @@ import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.ViewController;
-
-import lineageos.providers.LineageSettings;
 
 import javax.inject.Inject;
 
@@ -71,11 +68,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
             if (StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
                 ArraySet<String> icons = StatusBarIconController.getIconHideList(
                         getContext(), newValue);
-                mBatteryHidden = icons.contains(mSlotBattery);
-                mView.setVisibility(mBatteryHidden ? View.GONE : View.VISIBLE);
-                if (!mBatteryHidden) {
-                    mView.updateBatteryStyle();
-                }
+                mView.setVisibility(icons.contains(mSlotBattery) ? View.GONE : View.VISIBLE);
             }
         }
     };
@@ -116,8 +109,6 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
     // Some places may need to show the battery conditionally, and not obey the tuner
     private boolean mIgnoreTunerUpdates;
     private boolean mIsSubscribedForTunerUpdates;
-
-    private boolean mBatteryHidden;
 
     @Inject
     public BatteryMeterViewController(
@@ -196,12 +187,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
 
     private void registerShowBatteryPercentObserver(int user) {
         mContentResolver.registerContentObserver(
-                LineageSettings.System.getUriFor(STATUS_BAR_SHOW_BATTERY_PERCENT),
-                false,
-                mSettingObserver,
-                user);
-        mContentResolver.registerContentObserver(
-                LineageSettings.System.getUriFor(STATUS_BAR_BATTERY_STYLE),
+                Settings.System.getUriFor(SHOW_BATTERY_PERCENT),
                 false,
                 mSettingObserver,
                 user);
@@ -227,9 +213,6 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
                     Settings.Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME)) {
                 // update the text for sure if the estimate in the cache was updated
                 mView.updatePercentText();
-            }
-            if (TextUtils.equals(uri.getLastPathSegment(), STATUS_BAR_BATTERY_STYLE)) {
-                mView.updateBatteryStyle();
             }
         }
     }
