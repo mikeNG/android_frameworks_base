123509a210195371b665ed5eb339202dd4b7d972
Michael Bestas <mkbestas@lineageos.org>
2023-10-11 17:52:27 +0300

    dnm match lineage
    Change-Id: Ic68f0d4c47b3d6a12849b7b44625c32e52a64dff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -27,6 +27,7 @@ import android.annotation.Nullable;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Parcelable;
